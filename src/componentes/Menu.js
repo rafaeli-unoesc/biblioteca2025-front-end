88860c63bbc7458ad4ce47d379eb1d920bbf01dd
rafaeli-unoesc <144069663+rafaeli-unoesc@users.noreply.github.com>
2025-05-13 22:04:30 -0300

    Criadas as páginas padrões para as interfaces

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -20,7 +20,7 @@ export default function Menu() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">
+                                <a className="nav-link active" aria-current="page" href="/">
                                     Home
                                 </a>
                             </li>
@@ -38,12 +38,12 @@ export default function Menu() {
                                     data-bs-toggle="dropdown"
                                     aria-expanded="false"
                                 >
-                                    Dropdown
+                                    Cadastros
                                 </a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li>
-                                        <a className="dropdown-item" href="#">
-                                            Action
+                                        <a className="dropdown-item" href="/categorias">
+                                            Categorias
                                         </a>
                                     </li>
                                     <li>
@@ -61,28 +61,9 @@ export default function Menu() {
                                     </li>
                                 </ul>
                             </li>
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link disabled"
-                                    href="#"
-                                    tabIndex={-1}
-                                    aria-disabled="true"
-                                >
-                                    Disabled
-                                </a>
-                            </li>
+
                         </ul>
-                        <form className="d-flex">
-                            <input
-                                className="form-control me-2"
-                                type="search"
-                                placeholder="Search"
-                                aria-label="Search"
-                            />
-                            <button className="btn btn-outline-success" type="submit">
-                                Search
-                            </button>
-                        </form>
+
                     </div>
                 </div>
             </nav>
