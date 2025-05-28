672cb5133ce8f0aa41d813fa2b3e946c5642d785
rafaeli-unoesc <144069663+rafaeli-unoesc@users.noreply.github.com>
2025-05-27 21:01:03 -0300

    Finalizado crud de categorias

--- src/paginas/FormCategoria.js
+++ src/paginas/FormCategoria.js
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import TituloCadastro from "../componentes/TituloCadastro";
 
 export default function FormCategoria() {
     const navegacao = useNavigate();
@@ -11,26 +12,67 @@ export default function FormCategoria() {
         navegacao('/listacategoria');
     };
 
+    const selecionar = async () => {
+        let { data } = await axios.get(`http://localhost:4000/categoria/${id}`);
+        setNomeCategoria(data.nomecategoria);
+    }
+
+    const alterar = async () => {
+        let body = {
+            "nomecategoria": nomecategoria
+        };
+
+        await axios.put(`http://localhost:4000/categoria/${id}`, body);
+        voltar();
+    }
+
+    const inserir = async () => {
+        let body = {
+            "nomecategoria": nomecategoria
+        };
+
+        await axios.post(`http://localhost:4000/categoria`, body);
+        voltar();
+    }
+
+    const salvar = async () => {
+        if (id) {
+            alterar();
+        }
+        else {
+            inserir();
+        }
+    }
+
     const excluir = async () => {
         await axios.delete(`http://localhost:4000/categoria/${id}`);
         voltar();
     }
 
+    useEffect(() => {
+        if (id) {
+            selecionar();
+        }
+    }, []);
+
     return (
         <>
-            <h1>Cadastros de categoria</h1>
+            <TituloCadastro id={id} titulo="categoria" />
 
             <form>
-                <div className="mb-3">
-                    <label className="form-label">
-                        Código
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={id}
-                    />
-                </div>
+                {id && (
+                    <div className="mb-3">
+                        <label className="form-label">
+                            Código
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={id}
+                        />
+                    </div>
+                )}
+
                 <div className="mb-3">
                     <label className="form-label">
                         Nome da categoria
@@ -42,7 +84,8 @@ export default function FormCategoria() {
                         onChange={(evento) => setNomeCategoria(evento.target.value)}
                     />
                 </div>
-                <button type="button" className="btn btn-primary">
+                <button type="button" className="btn btn-primary"
+                    onClick={() => salvar()}>
                     Salvar
                 </button>
                 <button type="button"
@@ -50,11 +93,13 @@ export default function FormCategoria() {
                     onClick={() => voltar()}>
                     Cancelar
                 </button>
-                <button type="button"
-                    className="btn btn-danger"
-                    onClick={() => excluir()}>
-                    Excluir
-                </button>
+                {id && (
+                    <button type="button"
+                        className="btn btn-danger"
+                        onClick={() => excluir()}>
+                        Excluir
+                    </button>
+                )}
             </form>
         </>
     );
